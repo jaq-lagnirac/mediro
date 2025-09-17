f63d61cda5e11505b245a4e7dcf06f5bc59fc1f8
Justin Caringal <jacfriday10th@gmail.com>
2025-09-17 14:06:50 -0500

    Completed PyInstaller metadata test

--- metadata-test.py
+++ metadata-test.py
@@ -3,11 +3,17 @@
 # testbed to extract EXIF tags
 
 import os
+import sys
 import time
 import exifread
-
-file_path = os.path.join('MEDIRO_requires_sorting',
-                         'WIN_20230426_14_15_23_Pro.jpg')
+# try:
+#     base_path = sys._MEIPASS # only found in PyInstaller
+# except AttributeError:
+#     base_path = os.curdir
+file_path = '20250214_183649.jpg'
+# file_path = os.path.join(base_path, file_path)
+# file_path = os.path.join('MEDIRO_requires_sorting',
+#                          'WIN_20230426_14_15_23_Pro.jpg')
 
 with open(file_path, 'rb') as handle:
     tags = exifread.process_file(handle)
@@ -15,6 +21,19 @@ with open(file_path, 'rb') as handle:
     for key, value in tags.items():
         print(f'{key:<30}{value}')
 
-    if 'EXIF DateTimeOriginal' in tags:
-        date_taken_str = str(tags['EXIF DateTimeOriginal'])
+    # search_value = 'Image DateTime'
+    search_value = 'EXIF DateTimeOriginal'
+    if search_value in tags:
+        date_taken_str = str(tags[search_value])
         date_taken_obj = time.strptime(date_taken_str, '%Y:%m:%d %H:%M:%S')
+        print(f'\n\n{date_taken_str}\n{date_taken_obj}')
+        
+        year = str(date_taken_obj.tm_year)
+        month = str(date_taken_obj.tm_mon).zfill(2)
+        day = str(date_taken_obj.tm_mday)
+        print(f'{year}_{month}_{day}')
+
+
+from time import sleep
+print('this is a test.')
+sleep(1000)
